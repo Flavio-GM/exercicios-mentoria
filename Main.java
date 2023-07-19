24cfb932f2dd08a0c4b39da175f6bc825ebb6cb8
Flávio Machado <flavio_gm@yahoo.com.br>
2023-07-19 10:08:00 -0300

    Exercicio atualizado

--- Main.java
+++ Main.java
@@ -7,13 +7,37 @@ import java.util.List;
 // Listar as pessoas que são do Brasil, em ordem de nome
 // Listar as pessoas que são do Brasil, em ordem de idade
 // Contar quantas pessoas tem mais de 45 anos
-
 // Calcular a média de idade das pessoas
+
+// filter --> Seleciona apenas uma parte dos dados, Para isso a expressão lambda deve retornar verdadeira ou falso.
+// map() --> Aplicado para trandorma os dados de entrada, gerando um novo conjunto de saída.
 
 public class Main {
     public static void main(String[] args) {
         List<Pessoa> pessoas = Dados.getPessoas();
 
-    }
+        // Listar todas as pessoas
+        pessoas.stream()
+                .forEach(System.out::println);
 
+        // Listar as 10 primeiras pessoas
+        pessoas.stream()
+                .limit(10)
+                .forEach(System.out::println);
+        
+        // Listar as pessoas que são do Brasil
+        System.out.println("\nPessoas do Brasil");
+        pessoas.stream()
+                .filter(p -> p.getPais()
+                .equals("Brazil"))
+                .forEach(System.out::println);
+        
+        // Listar as pessoas do Brasil com o nome em Uppercase e suas idades
+        System.out.println("\nPessoas do Brasil com nome maiusculo e suas idades");
+        pessoas.stream()
+                .filter(p -> p.getPais()
+                .equals("Brazil"))
+                .map(p -> p.getNome().toUpperCase() + ", " + p.getIdade())
+                .forEach(System.out::println);
+    }
 }
